09a353ef4cd77b724e8ab2ebcc389f437a56c4d4
noliverio <nick@nicholasoliverio.com>
2018-02-19 20:56:28 -0500

    worked on new better structure for set2 and onwards

---
+++ set2/challenge_9/challenge_9.go
@@ -0,0 +1,7 @@
+package challenge_9
+
+import "fmt"
+
+func TestFunction(vari []byte) {
+	fmt.Printf("%s\n", vari)
+}
---
+++ set2/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"friendly-doodle/set2/challenge_9"
+)
+
+func main() {
+	fmt.Println("vim-go")
+	challenge_9.TestFunction([]byte("abc"))
+}
