c7a086fd78d4be7418539c6d4697322b231e0e14
noliverio <nick@nicholasoliverio.com>
2018-02-25 19:12:21 -0500

    Pad function from 9 was assuming only one block. Not as useful as an entire message.

--- set2/challenge_9/challenge_9.go
+++ set2/challenge_9/challenge_9.go
@@ -2,25 +2,48 @@ package challenge_9
 
 import "fmt"
 
-func Pad(message []byte, blockLength int) []byte {
-	// apply a padding to the end of the message such that the number of bytes in the padding
+// This assumes that the message is only one block long.
+// Not that usefull. lets rework to pad message of n blocks
+func Pad(block []byte, blockLength int) []byte {
+	// apply a padding to the end of the block such that the number of bytes in the padding
 	// is the byte used for the padding
-	bytesNeeded := blockLength - len(message)
+	bytesNeeded := blockLength - len(block)
 	paddingByte := byte(bytesNeeded)
-	paddedMessage := make([]byte, blockLength)
+	paddedBlock := make([]byte, blockLength)
 
 	for i := 0; i < blockLength; i++ {
-		if i < len(message) {
-			paddedMessage[i] = message[i]
+		if i < len(block) {
+			paddedBlock[i] = block[i]
 		} else {
-			paddedMessage[i] = paddingByte
+			paddedBlock[i] = paddingByte
 
 		}
 	}
 
+	return paddedBlock
+}
+
+func PadMessage(message []byte, blockLength int) []byte {
+	if len(message)%blockLength == 0 {
+		return message
+	}
+	numberOfBlocks := (len(message) / blockLength) + 1
+	lastBlockStart := (numberOfBlocks - 1) * blockLength
+	paddedMessage := make([]byte, numberOfBlocks*blockLength)
+	lastBlock := Pad(message[lastBlockStart:], blockLength)
+
+	for i := 0; i < lastBlockStart; i++ {
+		paddedMessage[i] = message[i]
+	}
+
+	for i := 0; i < len(lastBlock); i++ {
+		paddedMessage[i+lastBlockStart] = lastBlock[i]
+	}
+
 	return paddedMessage
+
 }
 
 func Main() {
-	fmt.Println(Pad([]byte("YELLOW SUBMARINE"), 20))
+	fmt.Println(PadMessage([]byte("YELLOW SUBMARINE"), 10))
 }
