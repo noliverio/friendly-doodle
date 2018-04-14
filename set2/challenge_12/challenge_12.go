789df9e400e53c62267717fd215bb285eff53269
noliverio <nick@nicholasoliverio.com>
2018-04-14 01:11:12 -0400

    commiting current state prior to refactor. Does not fully decrypt.

--- set2/challenge_12/challenge_12.go
+++ set2/challenge_12/challenge_12.go
@@ -1,8 +1,9 @@
 package challenge_12
 
 import (
+	"bytes"
 	"encoding/base64"
-	//	"fmt"
+	"fmt"
 
 	"friendly-doodle/utils"
 
@@ -11,37 +12,136 @@ import (
 	"math/rand"
 )
 
-func encrypt(msg []byte, key []byte) []byte {
+var key = make([]byte, 16)
+var _, _ = rand.Read(key)
 
-	paddedMsg := challenge_9.PadMessage(randomizedMsg, 16)
+// Encrypt takes a message and key, pads it appropriately, encrypts the message, and returns the cipher text
+func Encrypt(msg []byte, key []byte) []byte {
+
+	paddedMsg := challenge_9.PadMessage(msg, len(key))
 	cipherText := make([]byte, len(paddedMsg))
 
 	cipherText = challenge_11.EcbEncrypt(paddedMsg, key)
 	return cipherText
 }
 
-func createAttackSlice(cipherText) []byte {
-	// could use blockMode, but won't acually use it in the attack
-	//	blockMode := challenge_11.DetectBlockMode(cipherText)
-	blockLength := findBlockLength()
+func encryptionWrapper(myMessage []byte) []byte {
+	secret_base64 := "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
+	secretMessage, err := base64.StdEncoding.DecodeString(secret_base64)
+	utils.Check(err)
+	key := []byte("12345678abcdefgh")
+	msg := append(myMessage, secretMessage...)
+	return Encrypt(msg, key)
+}
 
-	attackString := make([]byte, blockLength-1)
-	for i := range attackString {
-		attackString[i] = byte("A")
+// A version of the encryption wrapper w/o the real secret
+func encryptionWrapperDev(myMessage []byte) []byte {
+	secretMessage := []byte("Hi secret message")
+	key := []byte("12345678abcdefgh")
+	msg := append(myMessage, secretMessage...)
+	return Encrypt(msg, key)
+}
+
+func buildAttackMap(blockLen int, blockNum int, known []byte) map[string]byte {
+
+	plainMap := buildMap(blockLen, known)
+	// encrypt each string to create an attack map
+	attackMap := make(map[string]byte)
+
+	for mapKey, value := range plainMap {
+		attackMap[string(encryptionWrapper([]byte(mapKey)))[((blockNum/2)*blockLen):(((blockNum/2)+1)*blockLen)]] = value
+		fmt.Printf("%v \n", encryptionWrapper([]byte(mapKey))[(blockNum*blockLen):((blockNum+1)*blockLen)])
+		fmt.Printf("%v \n", encryptionWrapper([]byte(mapKey)))
+	}
+	return attackMap
+
+}
+
+// buildMap takes the block length, and the known bytes to create a map of
+// all possible outputs from the oracle
+func buildMap(blockLen int, known []byte) map[string]byte {
+	// create a base string of one less than the block length
+	// append each byte to the base string and add the new string and byte to a string to byte map
+	plainMap := make(map[string]byte)
+	var baseString string
+
+	if blockLen <= len(known) {
+		baseString = string(known[len(known)%blockLen+1:])
+	} else {
+		neededPrepends := blockLen - len(known) - 1
+
+		var buffer bytes.Buffer
+		for count := 0; count < neededPrepends; count++ {
+			buffer.WriteString("\x00")
+		}
+		prependString := buffer.String()
+		baseString = prependString + string(known)
+	}
+	var byt byte
+	for byt = 0; byt <= 127; byt++ {
+		plainMap[baseString+string(byt)] = byt
+	}
+
+	return plainMap
+}
+
+// create AttackStrings takes the secret mesage and appends bytes to the begining
+func createAttackStrings(blockLen int) [][]byte {
+	attackPrefixes := make([][]byte, blockLen)
+	for i := 0; i < blockLen; i++ {
+		attackPrefixes[i] = make([]byte, i)
+	}
+
+	attackStrings := make([][]byte, len(attackPrefixes))
+
+	for pos, str := range attackPrefixes {
+		attackStrings[pos] = encryptionWrapper(str)
+	}
+
+	return attackStrings
+}
+
+func attack() {
+	// create the set of attack strings
+	// iteratively decrypt the secret message byte by byte
+
+	blockSize := 16
+	blockNum := 0
+	attackStringSelector := 15
+
+	known := make([]byte, 0)
+
+	attackStrings := createAttackStrings(blockSize)
+
+	for range attackStrings[0] {
+		// decrypt the first byte
+		attackMap := buildAttackMap(blockSize, blockNum, known)
+
+		attackedBlock := string(attackStrings[attackStringSelector][(blockNum * blockSize):((blockNum + 1) * blockSize)])
+		byt, ok := attackMap[attackedBlock]
+		if ok {
+			known = append(known, byt)
+			fmt.Println(byt)
+		} else {
+			fmt.Println("AttackedBlock:")
+			fmt.Printf("%v, \n", []byte(attackedBlock))
+			//			for key := range attackMap {
+			//				fmt.Printf("%v \n", []byte(key))
+			//			}
+		}
+
+		if attackStringSelector == 0 {
+			attackStringSelector = 15
+			blockNum++
+			fmt.Println("BlockNum: ")
+			fmt.Println(blockNum)
+		} else {
+			attackStringSelector--
+		}
+		fmt.Printf("%s \n", known)
 	}
 }
 
-func main() {
-	secret_base64 := "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
-	secret, err := base64.StdEncoding.DecodeString(secret_base64)
-	utils.Check(err)
-	key := make([]byte, 16)
-	_, err = rand.Read(key)
-	utils.Check(err)
-
-	attack_slice := []byte("AAAAAAAAAAAAA")
-	msg := append(attack_slice, secret...)
-
-	encrypt(msg, key)
-
+func Main() {
+	attack()
 }
