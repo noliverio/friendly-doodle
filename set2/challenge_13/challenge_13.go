3fe5f4122f1b0711ad789c6e795d49f182d307b2
noliverio <nick@nicholasoliverio.com>
2018-04-16 20:43:51 -0400

    commentary in challege 13

--- set2/challenge_13/challenge_13.go
+++ set2/challenge_13/challenge_13.go
@@ -1,6 +1,11 @@
+// This challenge seems like it is mostly going to take place in the utils section.
+
 package challenge_13
 
-import "fmt"
+import (
+	"fmt"
+	"friendly-doodle/utils"
+)
 
 func Main() {
 	fmt.Println("vim-go")
